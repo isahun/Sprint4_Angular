032468d16fd4a1a7c6dfc32a4657dfe421aa0767
Irene <isahun7@gmail.com>
2026-03-16 18:21:03 +0100

    feat: implement signal-based reactivity and update root component

--- meu-primer-angular/src/app/app.ts
+++ meu-primer-angular/src/app/app.ts
@@ -8,5 +8,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  //declare Signal
   protected readonly title = signal('meu-primer-angular');
+  
+  //method to update Signal
+  changeTitle() {
+    this.title.set('Títol Canviat per Signal!'); // Utilitzar .set() per actualitzar la Signal
+  }
 }
